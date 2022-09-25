2707ffc360da3efdda4839e40eae5c919ede26eb
deimos <marcobavagnolidev@gmail.com>
2022-09-25 12:32:07 +0200

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,164 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_metaball/flutter_metaball.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController? controller;
+  int voxelSize = 2;
+  int nMetaball = 5;
+  double minRadius = 20;
+  double maxRadius = 30;
+  double minSpin = 1;
+  double maxSpin = 3;
+  double width = 400;
+  double height = 400;
+  bool attraction = false;
+
+  List<MetaballParams> metaballs = [];
+
+  /// define the spin of each metaball
+  List<Offset> spins = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // set random metaball
+    metaballs = List.generate(
+      nMetaball,
+      (index) => MetaballParams(
+        Offset(Random().nextDouble() * width, Random().nextDouble() * height),
+        Random().nextDouble() * (maxRadius - minRadius) + minRadius,
+      ),
+    );
+
+    // set random spin
+    spins = List.generate(
+        nMetaball,
+        (index) => Offset(
+              (Random().nextDouble() * (maxSpin - minSpin) - minSpin) *
+                  (Random().nextInt(2) * 2 - 1), // <- random sign
+              (Random().nextDouble() * (maxSpin - minSpin) - minSpin) *
+                  (Random().nextInt(2) * 2 - 1), // <- random sign
+            ));
+
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1))
+          ..addListener(() {
+            // check if the metaball is going out of view
+            for (int n = 0; n < nMetaball; ++n) {
+              double dx = metaballs[n].center.dx + spins[n].dx;
+              double dy = metaballs[n].center.dy + spins[n].dy;
+              if (dx > width || dx <= 0) {
+                spins[n] = Offset(spins[n].dx * -1, spins[n].dy);
+              }
+              if (dy > height || dy <= 0) {
+                spins[n] = Offset(spins[n].dx, spins[n].dy * -1);
+              }
+
+              metaballs[n].center += spins[n];
+            }
+            if (mounted) setState(() {});
+          });
+    controller?.repeat();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  // return the angle between 0 and 2pi
+  double myAtan2(double y, double x) {
+    double ret;
+    ret = atan2(y, x);
+    if (ret < 0) ret += pi * 2;
+    return ret;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Metaball example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: width,
+            height: height,
+            child: Listener(
+              // add a metaball under the pointer when pressed,
+              // remove it when released
+              onPointerDown: (e) {
+                metaballs.add(
+                  MetaballParams(e.localPosition, 15),
+                );
+              },
+              onPointerUp: (e) => metaballs.removeLast(),
+              onPointerMove: (e) {
+                metaballs.last.center = e.localPosition;
+
+                // attract the balls to pointer position?
+                if (attraction) {
+                  for (int n = 0; n < nMetaball; ++n) {
+                    double angle = myAtan2(
+                        e.localPosition.dy - metaballs[n].center.dy,
+                        e.localPosition.dx - metaballs[n].center.dx);
+                    spins[n] = Offset(
+                      cos(angle) * (Random().nextDouble() * maxSpin + minSpin),
+                      sin(angle) * (Random().nextDouble() * maxSpin + minSpin),
+                    );
+                  }
+                }
+              },
+              child: Metaball(
+                balls: metaballs,
+                voxel: voxelSize,
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Checkbox(
+                value: attraction,
+                onChanged: (_) => attraction = !attraction,
+              ),
+              const Text('pointer attraction'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/flutter_metaball.dart
@@ -0,0 +1,96 @@
+library flutter_metaball;
+
+import 'package:flutter/material.dart';
+
+class MetaballParams {
+  /// Position
+  Offset mCenter;
+  Offset get center => mCenter;
+  set center(Offset center) {
+    mCenter = center;
+  }
+
+  /// The strenght defines also the radius
+  final double radius;
+
+  MetaballParams(this.mCenter, this.radius);
+}
+
+class Metaball extends StatelessWidget {
+  /// the more voxel is the more inaccurate will be.
+  /// Define the square edge where computation will be made
+  final int voxel;
+
+  final List<MetaballParams> balls;
+
+  const Metaball({
+    super.key,
+    required this.voxel,
+    required this.balls,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: CustomPaint(
+        painter: MetaballPainter(balls: balls, voxel: voxel),
+      ),
+    );
+  }
+}
+
+class MetaballPainter extends CustomPainter {
+  /// the more is voxel the more inaccurate will be.
+  /// Define the square edge where computation will be made
+  final int voxel;
+
+  final List<MetaballParams> balls;
+
+  MetaballPainter({
+    this.voxel = 20,
+    required this.balls,
+  })  : assert(voxel > 0, 'voxel must be >0 !'),
+        assert(balls.isNotEmpty, 'some metaball(s) should be provided!');
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    int rows = 0;
+    int cols = 0;
+    List<List<double>> matrix = List.generate(size.height ~/ voxel + 1,
+        (index) => List.generate(size.width ~/ voxel + 1, (index) => 0.0));
+    final Paint paint = Paint();
+
+    paint.color = const Color(0xFFe0e0e0);
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+
+    double f = 0;
+    for (int y = 0; y < size.height; y += voxel, ++cols) {
+      rows = 0;
+      for (int x = 0; x < size.width; x += voxel, ++rows) {
+        f = 0;
+        for (int n = 0; n < balls.length; ++n) {
+          f += balls[n].radius /
+              (Offset(balls[n].center.dx - x, balls[n].center.dy - y).distance +
+                  0.00001);
+          if (matrix[cols][rows] < f) matrix[cols][rows] = f;
+        }
+
+        if (matrix[cols][rows] > 1.0) {
+          paint.color = Color.fromARGB(
+              255, 0, 192 + (64 * matrix[cols][rows]).toInt(), 0);
+          canvas.drawCircle(Offset(1.0 * x, 1.0 * y), 2, paint);
+        }
+      }
+    }
+
+    paint.color = Colors.yellow;
+    paint.strokeWidth = 1.0;
+    paint.style = PaintingStyle.stroke;
+    for (int n = 0; n < balls.length; n++) {
+      canvas.drawCircle(balls[n].center, balls[n].radius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
